93550a3289a189afbbb37290bfba3fb820b15554
samchamper <samchamper@gmail.com>
2018-04-06 22:20:07 -0700

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,48 @@
+# To run from source: 
+#    make install
+#    make run 
+# 
+# Many recipes need to be run in the virtual environment, 
+# so run them as $(INVENV) command
+INVENV = . env/bin/activate ;
+
+##
+##  Virtual environment
+##     
+PYVENV = python3 -m venv
+env:
+	$(PYVENV)  env
+	($(INVENV) pip install -r requirements.txt )
+
+install:  env  credentials
+
+# 'make run' runs the built-in Flask server.  Useful for debugging,
+# but not suitable for long-running service.
+#
+credentials:  source/credentials.ini
+source/credentials.ini:
+	echo "You just install the database and credentials.ini for it"
+
+run:	env credentials
+	$(INVENV) cd source; python3 flask_main.py
+
+##
+## Preserve virtual environment for git repository
+## to duplicate it on other targets
+##
+dist:	env
+	$(INVENV) pip freeze >requirements.txt
+
+# 'clean' and 'veryclean' are typically used before checking 
+# things into git.  'clean' should leave the project ready to 
+# run, while 'veryclean' may leave project in a state that 
+# requires re-running installation and configuration steps
+# 
+clean:
+	cd source; rm -f *.pyc
+	cd source; rm -rf __pycache__
+
+veryclean:
+	make clean
+	rm -rf env
+
---
+++ source/flask_main.py
@@ -0,0 +1,194 @@
+"""
+Author: Sam Champer
+Flask web app connects to Mongo database.
+Keep a simple list of dated memoranda.
+
+Representation conventions for dates:
+   - Uses arrow objects to manipulate dates, but iso date strings for all other
+     purposes. Dates shown to user will be formatted by a humanize function.
+"""
+
+import flask
+from flask import request
+from flask import url_for
+
+import logging
+import sys
+import arrow
+
+# Mongo database
+from pymongo import MongoClient
+
+import config
+CONFIG = config.configuration()
+
+
+MONGO_CLIENT_URL = "mongodb://{}:{}@{}:{}/{}".format(
+    CONFIG.DB_USER,
+    CONFIG.DB_USER_PW,
+    CONFIG.DB_HOST,
+    CONFIG.DB_PORT,
+    CONFIG.DB)
+
+print("Using URL '{}'".format(MONGO_CLIENT_URL))
+
+###
+# Globals
+###
+app = flask.Flask(__name__)
+app.secret_key = CONFIG.SECRET_KEY
+
+####
+# Database connection per server process
+###
+try:
+    dbclient = MongoClient(MONGO_CLIENT_URL)
+    db = getattr(dbclient, str(CONFIG.DB))
+    collection = db.memos
+except:
+    print("Failure opening database. Is Mongo running? Correct password?")
+    sys.exit(1)
+
+
+###
+# Pages
+###
+@app.route("/")
+@app.route("/index")
+def index():
+    app.logger.debug("Main page entry")
+    return flask.render_template('index.html')
+
+
+# A function to display memos on page load
+@app.route("/_disp")
+def disp():
+    app.logger.debug("Initializing memos.")
+    result = {"memos": get_memos()}
+    return flask.jsonify(result=result)
+
+
+# Create a new memo
+@app.route("/_create")
+def create():
+    app.logger.debug("Creating memo")
+    # Get the memo contents from javascript side.
+    contents = request.args.get('contents')
+    date = request.args.get('date')
+    # Find a new index for the next memo:
+    cur_index = memo_idx()
+    try:
+        # Try block is for checking if date is valid.
+        valid = arrow.get(date, 'YYYY-M-D')
+    except:
+        er = "Not a good time"
+        app.logger.debug(er)
+        result = {"error": er}
+        return flask.jsonify(result=result)
+
+    new = {"type": "dated_memo",
+           "date": date,
+           "text": contents,
+           "index": cur_index}
+    collection.insert(new)
+    result = {"memos": get_memos()}
+    return flask.jsonify(result=result)
+
+
+# Delete a memo
+@app.route("/_del")
+def delete():
+    # Get the index for the memo to delete from js:
+    id = request.args.get('id', type=int)
+    app.logger.debug("Deleting memo")
+    # Delete the memo:
+    del_memo(id)
+    # Send the remaining memos over to js:
+    result = {"memos": get_memos()}
+    return flask.jsonify(result=result)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.debug("Page not found")
+    return flask.render_template('page_not_found.html',
+                                 badurl=request.base_url,
+                                 linkback=url_for("index")), 404
+
+
+##############
+# Functions available to the page code above
+##############
+def humanize_arrow_date(date):
+    """
+    Date is internal UTC ISO format string.
+    Output should be "today", "yesterday", "in 5 days", etc.
+    Arrow will try to humanize down to the minute, so we
+    need to catch 'today' as a special case.
+    """
+    try:
+        then = arrow.get(date, 'YYYY-M-D').to('local')
+        # If we are in a tz less than Greenwhich (West), then the memos are getting a date
+        # that is one day too soon, so shift forward a day if we are in such a time zone.
+        # the [-6]th element of the arrow time string is the sign on the timezone segment of the time.
+        # Thus, if it is "-", we need to shift forward one day.
+        East_of_Greenwhich = arrow.now().isoformat()[-6]
+        if East_of_Greenwhich == "-":
+            then = then.shift(days=+1)
+        now = arrow.utcnow().to('local')
+
+        # Some special cases for the humanize functions: today, tomorrow, yesterday.
+        if then.date() == now.date():
+            human = "Today"
+        elif str(then.date() - now.date()) == "1 day, 0:00:00":
+            human = "Tomorrow"
+        elif str(now.date() - then.date()) == "1 day, 0:00:00":
+            human = "Yesterday"
+        else:
+            human = then.humanize()
+    except:
+        human = date
+    return human
+
+
+def get_memos():
+    """
+    Returns all memos in the database, in a form that
+    can be inserted directly in the 'session' object,
+    and in sorted order.
+    """
+    records = []
+    for record in collection.find({"type": "dated_memo"}):
+        record['a_date'] = arrow.get(record['date'], 'YYYY-M-D').isoformat()
+        del record['_id']
+        record['disp_date'] = humanize_arrow_date(record.get('date'))
+        records.append(record)
+    # Sort the records by arrow date:
+    records.sort(key=lambda i: arrow.get(i['a_date']))
+    return records
+
+
+def memo_idx():
+    # The easiest way to delete memos is if each has
+    # an index that is unique for each memo.
+    # This might be slow for a very large database?
+    new_index = 0
+    for record in collection.find():
+        if record["index"] > new_index:
+            new_index = record["index"]
+    new_index += 1
+    return new_index
+
+
+def del_memo(idx):
+    """
+    Deletes a memo with a specified index from the database.
+    """
+    for record in collection.find({"index": idx}):
+        collection.delete_one(record)
+
+
+if __name__ == "__main__":
+    app.debug = CONFIG.DEBUG
+    app.logger.setLevel(logging.DEBUG)
+    app.run(port=CONFIG.PORT, host="0.0.0.0")
